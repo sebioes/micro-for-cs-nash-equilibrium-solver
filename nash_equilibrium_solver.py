c6ce05f87c6486a19ca20b84b960aef3f56f463b
sebioes <121304225+sebioes@users.noreply.github.com>
2024-03-14 11:55:37 +0100

    init

---
+++ nash_equilibrium_solver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------------------------------
+# Nash Equilibrium Solver for Finite Two-Person Games
+# --------------------------------------------------------------------------------
+# Description:
+# This script is designed to find the Nash equilibrium in finite two-person games.
+# It is developed as part of the coursework for the course "Economics: Micro for Computer Science"
+#
+# Authors:
+# - Sebastian Oes, sebastian.oes@student.unisg.ch
+# - Timo Stricker, timo.stricker@student.unisg.ch
+#
+# University: University of St.Gallen
+# Course: Economics: Micro for Computer Science
+# --------------------------------------------------------------------------------
+
